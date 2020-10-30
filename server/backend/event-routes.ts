107d41807c064aae7b8161d90988197dfd7c0670
AlonBru <indyalon@gmail.com>
2020-10-30 13:00:39 +0200

    update

--- server/backend/event-routes.ts
+++ server/backend/event-routes.ts
@@ -27,43 +27,57 @@ interface Filter {
   offset: number;
 }
 
-router.get("/all", (req: Request, res: Response) => {
+router.get('/all', (req: Request, res: Response) => {
+  res.send('/all')
+    
 });
 
-router.get("/all-filtered", (req: Request, res: Response) => {
+router.get('/all-filtered', (req: Request, res: Response) => {
+  res.send('/all-filtered')
 });
 
-router.get("/by-days/:offset", (req: Request, res: Response) => {
+router.get('/by-days/:offset', (req: Request, res: Response) => {
+  res.send('/by-days/:offset')
 });
 
-router.get("/by-hours/:offset", (req: Request, res: Response) => {
+router.get('/by-hours/:offset', (req: Request, res: Response) => {
+  res.send('/by-hours/:offset')
 });
 
-router.get("/today", (req: Request, res: Response) => {
+router.get('/today', (req: Request, res: Response) => {
+  res.send('/today')
 });
 
-router.get("/week", (req: Request, res: Response) => {
+router.get('/week', (req: Request, res: Response) => {
+  res.send('/week')
 });
 
-router.get("/retention", (req: Request, res: Response) => {
+router.get('/retention', (req: Request, res: Response) => {
+  res.send('/retention')
 });
-router.get("/:eventId",(req : Request, res : Response) => {
+router.get('/:eventId',(req : Request, res : Response) => {
+  res.send('/:eventId')
 });
 
-router.post("/", (req: Request, res: Response) => {
+router.post('/', (req: Request, res: Response) => {
+  res.send('/')
 });
 
 router.get('/chart/os/:time',(req: Request, res: Response) => {
+  res.send('/chart/os/:time')
 })
 
   
 router.get('/chart/pageview/:time',(req: Request, res: Response) => {
+  res.send('/chart/pageview/:time')
 })
 
 router.get('/chart/timeonurl/:time',(req: Request, res: Response) => {
+  res.send('/chart/timeonurl/:time')
 })
 
 router.get('/chart/geolocation/:time',(req: Request, res: Response) => {
+  res.send('/chart/geolocation/:time')
 })
 
 
