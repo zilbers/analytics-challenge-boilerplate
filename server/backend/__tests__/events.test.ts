adad9f5b6c9fbe441b075d3a84675629927e8f0b
AlonBru <indyalon@gmail.com>
2020-11-01 05:29:54 +0200

    Update events.test.ts

--- server/backend/__tests__/events.test.ts
+++ server/backend/__tests__/events.test.ts
@@ -37,7 +37,7 @@ describe("main test", () => {
 
   it("can get all events", async () => {
     const { body: allEvents } = await request(app).get("/events/all").expect(200);
-    expect(allEvents.length).toBe(250);
+    expect(allEvents.length).toBe(300);
   });
 
   it("getting all events from the server must return array of event types", async () => {
@@ -45,40 +45,33 @@ describe("main test", () => {
     expect(isEventArray(allEvents)).toBe(true);
   });
 
-  it("can post new event", async () => {
-    await request(app).post("/events").send(mockData.events[0]).expect(200);
-    const { body: allEvents2 } = await request(app).get("/events/all").expect(200);
-    expect(allEvents2.length).toBe(251);
-    expect(allEvents2[250].date).toBe(mockData.events[0].date);
-    expect(allEvents2[250].os).toBe(mockData.events[0].os);
-  });
-
   it("can get unique sessions count by day", async () => {
     const { body: sessionsByDays } = await request(app).get("/events/by-days/0").expect(200)
 
     expect(sessionsByDays.length).toBe(7)
-    expect(sessionsByDays.reduce((sum: number, day: {date: string; count: number}) => sum += day.count, 0)).toBe(47)
-    expect(sessionsByDays[0].count).toBe(8);
+    expect(sessionsByDays.reduce((sum: number, day: {date: string; count: number}) => sum += day.count, 0)).toBe(145
+      )
+    expect(sessionsByDays[0].count).toBe(19);
 
     const { body: sessionsByDays2 } = await request(app).get("/events/by-days/7").expect(200)
 
     expect(sessionsByDays2.length).toBe(7)
-    expect(sessionsByDays2.reduce((sum: number, day: {date: string; count: number}) => sum += day.count, 0)).toBe(56)
-    expect(sessionsByDays2[0].count).toBe(8);
-    expect(sessionsByDays2[1].count).toBe(8);
-    expect(sessionsByDays2[6].count).toBe(8);
+    expect(sessionsByDays2.reduce((sum: number, day: {date: string; count: number}) => sum += day.count, 0)).toBe(78)
+    expect(sessionsByDays2[0].count).toBe(11);
+    expect(sessionsByDays2[1].count).toBe(9);
+    expect(sessionsByDays2[6].count).toBe(14);
   });
 
   it("can get unique sessions count by hour", async () => {
     const { body: sessionsByHours } = await request(app).get("/events/by-hours/0").expect(200)
 
     expect(sessionsByHours.length).toBe(24)
-    expect(sessionsByHours.reduce((sum: number, day: {date: string; count: number}) => sum += day.count, 0)).toBe(2)
+    expect(sessionsByHours.reduce((sum: number, day: {date: string; count: number}) => sum += day.count, 0)).toBe(7)
 
     const { body: sessionsByHours2 } = await request(app).get("/events/by-hours/2").expect(200)
 
     expect(sessionsByHours2.length).toBe(24)
-    expect(sessionsByHours2.reduce((sum: number, day: {date: string; count: number}) => sum += day.count, 0)).toBe(8)
+    expect(sessionsByHours2.reduce((sum: number, day: {date: string; count: number}) => sum += day.count, 0)).toBe(25)
   });
 
   it("retention cohort", async () => {
@@ -143,5 +136,13 @@ describe("main test", () => {
     expect(events.events[0].date).toBeGreaterThan(events.events[4].date)
     expect(events2.events[1].date).toBeGreaterThan(events2.events[0].date)
   })
+
+  it("can post new event", async () => {
+    await request(app).post("/events").send(mockData.events[0]).expect(200);
+    const { body: allEvents2 } = await request(app).get("/events/all").expect(200);
+    expect(allEvents2.length).toBe(301);
+    expect(allEvents2[300].date).toBe(mockData.events[0].date);
+    expect(allEvents2[300].os).toBe(mockData.events[0].os);
+  });
 
 })
